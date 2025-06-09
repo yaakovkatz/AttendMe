f7b568aca8c22f7782a46426623f3d14e1e17fd6
yaakovkatz <0527134@gmail.com>
2025-06-09 18:15:06 +0300

    fix: align tensorflow version for Render

--- app.py
+++ app.py
@@ -912,4 +912,7 @@ threading.Thread(target=background_load_people, daemon=True).start()
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='127.0.0.1', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
